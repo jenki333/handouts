e7e347df923832b85682c0d24ae5dbbd908b01b7
jenki333 <jenki333@msu.edu>
2017-07-18 14:46:22 +0000

    SESYNC

--- worksheet-2.sh
+++ worksheet-2.sh
@@ -2,13 +2,13 @@
 
 ## Configure git
 
-git config --global user.name ...
-git config --global user.email ...
+git config --global user.name jenki333
+git config --global user.email jenki333@msu.edu
 
 ## Change the "origin" remote URL and push
-git remote set-url origin ...
-git push --all
+git remote set-url origin github.com/jenki333/handouts.git
+
 
 ## Set upstream
-#git remote add upstream https://github.com/sesync-ci/handouts
-#git pull upstream master
+git remote add upstream https://github.com/sesync-ci/handouts
+git pull upstream master
